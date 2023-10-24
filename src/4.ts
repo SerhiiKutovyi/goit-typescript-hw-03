2fe782be3443fdfbb4badcd6ebd696a22be5eb26
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2023-10-24 18:50:30 +0300

    T4-?

--- src/4.ts
+++ src/4.ts
@@ -10,17 +10,56 @@ class Key {
   }
 }
 
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  private tenants: Person[] = [];
+  constructor(public door: boolean, public key: Key) {}
+
+  comeIn(): any {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`entered the house.`);
+    } else {
+      console.log('The door is closed. Cannot enter.');
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  private isOpen: boolean = false;
+
+  constructor(door: boolean, key: Key){
+   super(door, key);
+  }
+
+  openDoor(key: Key) {
+    if (!this.isOpen && key.getSignature() === this.key.getSignature()) {
+      this.isOpen = true;
+      console.log('Door is opened.');
+    } else {
+      console.log('Invalid key or the door is already open.');
+    }
+  }
+}
+
 const key = new Key();
 
-console.log(key);
+const house = new MyHouse(key);
+const person = new Person(key);
+console.log(person);
 
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+house.openDoor(person.getKey());
 
-// house.openDoor(person.getKey());
-
-// house.comeIn(person);
+house.comeIn(person);
 
 
 export {};
